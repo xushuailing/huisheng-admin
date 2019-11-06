4c214759a91a121c18aadc8c585860fe2472327d
arieslu <lujunjiebreak@163.com>
2019-11-06 18:30:37 +0800

    feat: 合并router
    n

--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,9 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import NProgress from 'nprogress';
 import 'normalize.css/normalize.css';
+import layoutView from './layout/index.vue';
 
 Vue.use(Router);
-
-const merchantRouter = Vue.extend({ render: (h) => h('router-view') });
-const AdminRouter = Vue.extend({ render: (h) => h('router-view') });
 
 const router = new Router({
   routes: [
@@ -16,45 +14,39 @@ const router = new Router({
       children: [
         {
           path: '/',
-          component: () => import(/* webpackChunkName: "main" */ './layout/main.vue'),
-          children: [
-            {
-              path: '/',
-              name: '首页',
-              component: () => import(/* webpackChunkName: "home" */ './pages/home/home.vue'),
-            },
-            {
-              path: '/merchant',
-              name: '首页',
-              redirect: '/merchant/list',
-              component: merchantRouter,
-              children: [
-                {
-                  path: 'list',
-                  name: '首页',
-                  component: () => import('./pages/merchant-admin/list.vue'),
-                },
-              ],
-            },
-            {
-              path: '/admin',
-              name: '系统管理',
-              component: AdminRouter,
-              redirect: '/admin/setting',
-              children: [
-                {
-                  path: 'setting',
-                  name: '平台管理',
-                  component: () => import(/* webpackChunkName: "setting" */ './pages/admin/setting/index.vue'),
-                },
-                {
-                  path: 'rootname',
-                  name: '总账号管理',
-                  component: () => import(/* webpackChunkName: "setting" */ './pages/admin/setting/root-name.vue'),
-                },
-              ],
-            },
-          ],
+          name: '首页',
+          component: () => import(/* webpackChunkName: "home" */ './pages/home/home.vue'),
+        },
+      ],
+    },
+    {
+      path: '/merchant',
+      name: '首页',
+      redirect: '/merchant/list',
+      component: layoutView,
+      children: [
+        {
+          path: 'list',
+          name: '首页',
+          component: () => import('./pages/merchant-admin/list.vue'),
+        },
+      ],
+    },
+    {
+      path: '/admin',
+      name: '系统管理',
+      component: layoutView,
+      redirect: '/admin/setting',
+      children: [
+        {
+          path: 'setting',
+          name: '平台管理',
+          component: () => import(/* webpackChunkName: "setting" */ './pages/admin/setting/index.vue'),
+        },
+        {
+          path: 'rootname',
+          name: '总账号管理',
+          component: () => import(/* webpackChunkName: "setting" */ './pages/admin/setting/root-name.vue'),
         },
       ],
     },
