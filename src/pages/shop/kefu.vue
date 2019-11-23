a22059cae0ddae4d409b9182d9d1e08b6438ea87
ryan liu <ryanliu@zego.im>
2019-11-23 14:29:19 +0800

    feat: 订单

--- src/pages/shop/kefu.vue
+++ src/pages/shop/kefu.vue
@@ -11,6 +11,27 @@
                   @pagination-onSlotClick="onSlotClick"
                   @emitTableHandlerClick="onTableHandlerClick">
     </sc-min-table>
+
+    <o-table :tableData="list11"
+            :thead="thead">
+      <template slot-scope="props">
+        <o-table-row>
+          <div slot="top_th">
+            订单号： 13123123, 订单号： 13123123, 订单号： 13123123
+          </div>
+          <div v-for="item in 3"
+               :key="item"
+               style="display:flex;justify-content: flex-start;align-items: center;">
+            <div style="flex:1;text-align:center;line-height: 40px;">{{props.row.key1}}</div>
+            <div style="flex:1;text-align:center;line-height: 40px;">{{props.row.key1}}</div>
+            <div style="flex:1;text-align:center;line-height: 40px;">{{props.row.key1}}</div>
+            <div style="flex:1;text-align:center;line-height: 40px;">{{props.row.key1}}</div>
+            <div style="flex:1;text-align:center;line-height: 40px;">{{props.row.key1}}</div>
+          </div>
+          <div slot="footer_th">213123</div>
+        </o-table-row>
+      </template>
+    </o-table>
   </div>
 </template>
 <script lang='ts'>
@@ -31,6 +52,24 @@ export default class MerchantInject extends Vue {
 
   columnsHandler: ScTable.ColumnsHandler = ['del'];
 
+  thead = ['退款单号', '原单号', '学校', '姓名', '身份证号'];
+
+  list11 = [
+    {
+      key1: 1,
+      key2: 1,
+      key3: 1,
+      key4: 1,
+      key5: 1,
+    },
+    {
+      key1: 2,
+      key2: 2,
+      key3: 2,
+      key4: 2,
+      key5: 2,
+    },
+  ];
 
   tableConfig = {
     api: this.$api.merchant.inject,
