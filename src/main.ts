cd24478f4ea70a2c463ec53d8a33621f9e49d8c3
许率领 <elfxsl0811@163.com>
2019-11-23 10:56:52 -0800

    feat: 添加 lodash

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import ElementUI from 'element-ui';
 import 'normalize.css/normalize.css';
 import 'element-ui/lib/theme-chalk/index.css';
 import VCharts from 'v-charts';
+import _ from 'lodash';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -20,6 +21,7 @@ Vue.config.productionTip = false;
 Vue.prototype.$utils = utils;
 Vue.prototype.$http = axios;
 Vue.prototype.$api = api;
+Vue.prototype._ = _;
 new Vue({
   router,
   store,
