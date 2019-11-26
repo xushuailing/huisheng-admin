8b408121f388596847878e720b1fef6b288f2047
许率领 <xushuailing@shencom.cn>
2019-11-26 16:26:45 +0800

    feat: 对接部分接口

--- src/pages/admin/merchant/shop-detail.vue
+++ src/pages/admin/merchant/shop-detail.vue
@@ -1,10 +1,110 @@
 <template>
-  <div class='merchant-shop'>
-
+  <div class='merchant-shop-detail pb-30'>
+    <el-tabs v-model="activeName">
+      <el-tab-pane label="店铺基本信息"
+                   lazy
+                   name="1">
+        <div class="p-30 bg-white border-radius-8">
+          <div v-for="item in detail"
+               :key="item.prop"
+               class="pb-15 mb-10">
+            <div v-if="item.type ==='img'"
+                 class="flex">
+              <div style="width:90px">{{item.label}}</div>
+              <img v-for="img in item.value"
+                   :width="item.width"
+                   :height="item.height"
+                   class="mr-10"
+                   :key="img"
+                   :src="img"
+                   alt="">
+            </div>
+            <div v-else
+                 class="flex">
+              <div style="width:90px">{{item.label}}</div>
+              <div>{{item.value}}</div>
+            </div>
+          </div>
+          <el-button type="primary"
+                     class="mt-20">
+            下架
+          </el-button>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="产品列表"
+                   lazy
+                   name="2">
+        <sc-min-table stripe
+                      ref="table"
+                      :columns-handler="columnsHandler"
+                      :columns="columns"
+                      :table-config="tableConfig">
+        </sc-min-table>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 <script lang='ts'>
 import { Component, Vue, Prop } from 'vue-property-decorator';
+import { ScTable } from '@/lib/@types/sc-table.d';
+
+const columns: ScTable.SetColumns = [
+  ['商品信息', 'title'],
+  ['商品类型', 'type'],
+  ['商品类别', 'type_title'],
+  ['销量', 'xnum', 100],
+  ['店铺名称', 'companyname'],
+];
+
 @Component
-export default class MerchantShop extends Vue {}
+export default class MerchantShopDetail extends Vue {
+  columns = this.$utils._SetTableColumns(columns);
+
+  columnsHandler = [{ name: 'editsales', title: '修改销量' }];
+
+  tableConfig = {
+    api: this.$api.admin.merchant.shop.goods,
+    index: {
+      shopid: 1,
+    },
+    handleWidth: 100,
+  };
+
+  activeName = '2';
+
+  detail = [
+    { label: '店铺名称：', value: '哎呦喂', prop: 'none1' },
+    { label: '店铺状态：', value: '正常', prop: 'none2' },
+    {
+      label: '店铺标志：',
+      value: ['http://placehold.it/200x200'],
+      prop: 'none3',
+      type: 'img',
+      width: '120px',
+      height: '120px',
+    },
+    { label: '掌柜名称：', value: '', prop: 'none4' },
+    { label: '服务电话：', value: '', prop: 'none5' },
+    { label: '开店时间：', value: '', prop: 'none6' },
+    { label: '营业时间：', value: '', prop: 'none7' },
+    { label: '经营品类：', value: '', prop: 'none8' },
+    { label: '配送方式：', value: '', prop: 'none9' },
+    {
+      label: '营业执照：',
+      value: ['http://placehold.it/200x201', 'http://placehold.it/200x202'],
+      prop: 'none10',
+      type: 'img',
+      width: '260px',
+      height: '150px',
+    },
+    {
+      label: '身份证：',
+      value: ['http://placehold.it/200x203', 'http://placehold.it/200x204'],
+      prop: 'none11',
+      type: 'img',
+      width: '260px',
+      height: '150px',
+    },
+  ];
+}
 </script>
---
+++ src/pages/admin/merchant/shop-join.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class='admin-setting'>
+
+    <div class=" p-15 bg-white mt-20 border-radius-4">
+      <div class="flex flex-ac flex-jsb font-18">
+        <span>商家入驻保证金额</span>
+        <el-button @click="onEditText('bond_price','商家入驻保证金额')"
+                   type="primary"
+                   plain>修改</el-button>
+      </div>
+      <div class="mt-15 font-text-regular"
+           style="white-space:pre-wrap">
+        <span>&yen;</span>
+        <span v-text="formData.bond_price||'暂无'"></span>
+      </div>
+
+    </div>
+    <div class="p-15 bg-white mt-20 border-radius-4">
+      <div class="flex flex-ac flex-jsb font-18">
+        <span>商家入驻客服电话</span>
+        <el-button @click="onEditText('bond_tel','商家入驻客服电话')"
+                   type="primary"
+                   plain>修改</el-button>
+      </div>
+      <div class="mt-15 font-text-regular"
+           style="white-space:pre-wrap">
+        <span v-text="formData.bond_tel||'暂无'"></span>
+      </div>
+    </div>
+    <div class=" p-15 bg-white mt-20 border-radius-4">
+      <div class="flex flex-ac flex-jsb font-18">
+        <span>商家入驻常见问题</span>
+        <el-button @click="onEditText('bond_questions','商家入驻常见问题')"
+                   type="primary"
+                   plain>修改</el-button>
+      </div>
+      <div class="mt-15 font-text-regular">
+        <span v-text="formData.bond_questions||'暂无'"></span>
+      </div>
+    </div>
+
+    <el-dialog :title="dialogConfig.title"
+               :visible.sync="dialogConfig.visible">
+      <div>
+        <el-input v-if="key=='bond_questions'"
+                  type="textarea"
+                  :autofocus="{ minRows: 4, maxRows: 6 }"
+                  :rows="4"
+                  placeholder="请输入内容"
+                  v-model="value">
+        </el-input>
+        <el-input v-else
+                  placeholder="请输入内容"
+                  maxlength="11"
+                  clearable
+                  v-model="value">
+          <template slot="prepend"
+                    v-if="key=='bond_price'">
+            &yen;
+          </template>
+        </el-input>
+
+      </div>
+      <span slot="footer">
+        <el-button @click="dialogConfig.visible = false">取 消</el-button>
+        <el-button type="primary"
+                   @click="onSubmit">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+<script lang='ts'>
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { obj } from '../../../lib/@types/sc-param';
+@Component
+export default class AdminSetting extends Vue {
+  dialogConfig = {
+    visible: false,
+    title: '修改内容',
+  };
+
+  key = '';
+
+  value = '';
+
+  textarea = '';
+
+  // imageList = [];
+
+  formData: obj = {
+    id: '',
+    bond_price: '',
+    bond_tel: '',
+    bond_questions: '',
+  };
+
+  get imgs() {
+    return this.$utils._DataTypeChange(this.formData.business_license);
+  }
+
+  mounted() {
+    this.getFormData();
+  }
+
+  async getFormData() {
+    const api = this.$api.admin.merchant.shop.shopJoinShow;
+    const { data } = await this.$http.get(api);
+    this.formData = data;
+  }
+
+  onEditText(prop: string, title: string, docs: string) {
+    this.dialogConfig.title = title;
+    this.dialogConfig.visible = true;
+    this.key = prop;
+    this.value = this.formData[prop];
+  }
+
+  onSubmit() {
+    const api = this.$api.admin.merchant.shop.shopJoinUpdate;
+    this.$http
+      .post(api, {
+        ...this.formData,
+        [this.key]: this.value,
+      })
+      .then((res) => {
+        this.formData[this.key] = this.value;
+      })
+      .finally(() => {
+        this.dialogConfig.visible = false;
+      });
+  }
+}
+</script>
