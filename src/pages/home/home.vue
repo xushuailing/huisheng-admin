02e21bde3e5a0bc5ca14a46464ee2ffc4f6021dd
许子禾 <elfxsl0811@163.com>
2019-12-07 18:29:18 +0800

    feat: 添加首页导航

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="home">
+    <div class="bg-warning-lighter p-20">
+      <div v-for="(router,i) in routers"
+           :key="i">
+        <div :key="i+ii"
+             v-for="(route,ii) in router">
+          <p>{{route.name}}</p>
+          <span class="pl-20"
+                :key="i+i+a"
+                v-for="(item,a) in route.children">
+            <router-link class="font-primary"
+                         :to="route.path+'/'+item.path">{{item.name}}</router-link>
+          </span>
+        </div>
+
+        <hr />
+      </div>
+
+    </div>
     <div>
       <c-statisticsfrom></c-statisticsfrom>
       <div class="mt-20">
         <c-line></c-line>
       </div>
       <div class="mt-20">
-       <c-admin-component></c-admin-component>
+        <c-admin-component></c-admin-component>
       </div>
       <el-row :gutter="20"
               class="mt-20">
@@ -27,9 +45,19 @@ import CLine from './components/line.vue';
 import CHistogram from './components/histogram.vue';
 import CMessages from './components/messages.vue';
 import CAdminComponent from './components/admin-component.vue';
+import adminRouter from '@/router/admin';
+import commonRouter from '@/router/common';
+import merchantRouter from '@/router/merchant';
 
 @Component({ components: { CStatisticsfrom, CLine, CHistogram, CMessages, CAdminComponent } })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  get routers() {
+    const admin = this.$utils._Clone(adminRouter) as any;
+    const common = this.$utils._Clone(commonRouter) as any;
+    const merchant = this.$utils._Clone(merchantRouter) as any;
+    return [admin, merchant, common];
+  }
+}
 </script>
 <style lang="scss" >
 .home {
